c7a102ad9cea75fc5be2e86c5f7e5b40d595e465
root <root@rhel-dev.m2mxs1ftgm3epoplnw5unkwzjb.bx.internal.cloudapp.net>
2022-07-03 16:01:28 +0000

    first start with networking programming, iterating over net_device structs

--- network-lkm/networking.c
+++ network-lkm/networking.c
@@ -1,8 +1,18 @@
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/netdevice.h>
 
 static int __init  networking_init(void) {
+
     pr_info("[*] loaded networking module\n");
+    struct net_device* dev;
+    read_lock(&dev_base_lock);
+    dev = first_net_device(&init_net);
+    while(dev) {
+        pr_info("dev name: %s with MTU: %u on IRQ: %d\n",dev->name,dev->mtu,dev->irq);
+        dev = next_net_device(dev);
+    }
+    read_unlock(&dev_base_lock);
     
     return 0;
 }
