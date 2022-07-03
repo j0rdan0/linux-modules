da483f4386d67c92f5ae79b72fed6910bc49945d
root <root@rhel-dev.m2mxs1ftgm3epoplnw5unkwzjb.bx.internal.cloudapp.net>
2022-07-03 15:11:41 +0000

    finished lkm test

---
+++ lkm-skeleton/load-module.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <errno.h>
+#define FINIT_SYSCALL 313
+#define MODULE_DEL_SYSCALL 176
+
+bool load_module();
+void unload_module();
+
+int main(int argc, char* argv[]) {
+        int s_value = 10;
+        if(load_module()) {
+                printf("[*]Failed loading module\n");
+                perror("err:");
+                return -1;
+        }
+        else {
+                printf("[*]Loaded module\n");
+                if (argc > 1) {
+                s_value = atoi(argv[1]);
+}
+                sleep(s_value);
+                unload_module();
+        }
+
+        return 0;
+}
+
+bool load_module() {
+
+
+//finit_module(int fd, const char* param_values,int flags)
+
+        int fd = open("skeleton.ko",O_RDONLY);
+        char null[] ="";
+        long ret = syscall(FINIT_SYSCALL,fd,null,0);
+        close(fd);
+        return (!ret) ? false: true;
+
+}
+
+void unload_module() {
+        char* MODULE = "skeleton";
+        syscall(MODULE_DEL_SYSCALL,MODULE,O_NONBLOCK);
+        printf("[*]Unloaded module\n");
+}
---
+++ lkm-skeleton/skeleton.c
@@ -0,0 +1,131 @@
+
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/miscdevice.h>
+#include <linux/delay.h>
+#include <linux/fs.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
+#include <linux/sched/signal.h>
+#include <linux/preempt.h>
+
+
+
+
+
+int test_open(struct inode * i, struct file * f) {
+    pr_info("***file opened\n");
+    return 0;
+}
+
+ssize_t test_read(struct file * f, char __user * us, size_t s, loff_t * lf) {
+    struct task_struct* proc;
+    char* buff = kmalloc(4096,GFP_KERNEL);
+    for_each_process(proc) {
+        strcat(buff,proc->comm);
+        strcat(buff," ");
+    }
+
+    if(copy_to_user(us,buff,4096)) {
+        pr_info("***failed to write to user space\n");
+    }
+    pr_info("***file read %d\n",s);
+    kfree(buff);
+    return s; // need to return 0 to have cat not loop ?
+}
+ssize_t test_write(struct file * f, const char __user * us, size_t s, loff_t * lf) {
+    char* buff = kmalloc(s,GFP_KERNEL);
+    if(copy_from_user(buff,us,(unsigned long)s)) {
+         pr_info("***failed to read from user space\n");
+    }
+    printk(buff);
+    pr_info("***wrote to file\n");
+    kfree(buff);
+    return s;
+}
+
+static const struct file_operations test_fops = {
+    .open = test_open,
+    .read = test_read,
+    .write = test_write,
+};
+
+static struct miscdevice test_device = { 
+    .minor = MISC_DYNAMIC_MINOR,
+    .name = "test_device",
+    .mode = 0666,
+    .fops = &test_fops, 
+};
+
+
+static int __init skeleton_init(void) {
+    pr_info("[*] loaded debug module\n");
+    msleep(1000);
+    pr_info("*** after sleeping\n");
+    printk("test_write at: 0x%px\n",test_write);
+    // get_nr_threads(p) - get threads no of task
+    if ( likely(in_task())) {
+         printk("current process: %s at 0x%px has stack at 0x%px at has %d threads \n",current->comm,current,current->stack,get_nr_threads(current));
+    }
+   else {
+    printk("running in interrupt context\n");
+   }
+
+    struct task_struct* t,*p;
+
+    // iterate over each thread
+    int t_no = 0;
+   do_each_thread(p,t){
+        task_lock(t);
+        printk("TGID: %d PID: %d Comm: %s\n",t->tgid,t->pid,t->comm);
+        t_no++;
+        task_unlock(t);
+   } while_each_thread(p,t);
+
+    printk("%d threads running\n",t_no);
+    
+    int ret;
+    ret = misc_register(&test_device);
+    if (ret != 0) {
+        pr_fmt("failed to register test device\n");
+        return ret;   
+    }
+    char msg[1024];
+    sprintf(msg,"registered device %s\n",test_device.name);
+    char* buff = kmalloc(128*1024,GFP_KERNEL);
+    printk("allocated 128kb\n");
+    kfree(buff);
+    printk("freed 128kb chunk\n");
+    static int step = 100;
+    void* chunk;
+    while (1) {
+        chunk = kmalloc(1024*step,GFP_KERNEL);
+        if(!chunk) {
+            pr_warn("[*]failed to allocate %d\n",(1024*step));
+            break;
+        }
+        pr_info("[*]requested %d and allocated %u at 0x%px\n",(1024*step),ksize(chunk),chunk);
+        kfree(chunk);
+        step += 200;
+        
+    }
+
+    printk(msg);
+    
+    return 0;
+}
+
+static void __exit skeleton_exit(void) {
+    pr_info("[*] unloaded debug module\n");
+    misc_deregister(&test_device);
+    pr_fmt("deregistered device\n");   
+  
+}
+
+module_init(skeleton_init);
+module_exit(skeleton_exit);
+
+
+MODULE_LICENSE("GPL");
